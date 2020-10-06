0458aa847dc50d70f9a0d760175aac7ebf87ae84
sofiamejiamuro <sofiamejiamuro@gmail.com>
2020-10-05 22:41:15 -0500

    updating project with build directory

---
+++ build/precache-manifest.331a9fd42230645f1382a9681e892d57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146796be369d14c91b966dc84ae9bf9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30fc2a46d4fbd79611fe",
+    "url": "/static/css/main.90b1d30a.chunk.css"
+  },
+  {
+    "revision": "9394425a6b8574bfd50e",
+    "url": "/static/js/2.87faeb51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87faeb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30fc2a46d4fbd79611fe",
+    "url": "/static/js/main.5034f7a1.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
